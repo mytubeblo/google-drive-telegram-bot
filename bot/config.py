b9a594b1eb9b39a00099a5622aab5b4b99e18aa2
mytubeblo <88610792+mytubeblo@users.noreply.github.com>
2021-08-08 15:42:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,7 +2,7 @@ class config:
     BOT_TOKEN = "1934439604:AAEk2Q1tDq8ecyFRpxqke1elkF3D56E5zD4"
     APP_ID = "7190364"
     API_HASH = "c1869de37aac697056d53479e5eb878f"
-    DATABASE_URL = "postgres://crdjfonecgolbe:11097813f149055345e3aca0cff3a03d9277b9e500433e1860948538d77d11b0@ec2-3-233-100-43.compute-1.amazonaws.com:5432/dcsv04801naoui"
+    DATABASE_URL = "postgres://cmgmwxydtjgifd:cbc80d5e94c7b51379232b552ee1dc8735d0a14ab7758ec6392a48d587095540@ec2-54-205-232-84.compute-1.amazonaws.com:5432/d64s4r1uusbfls"
     SUDO_USERS = "1328491403" # Sepearted by space.
     SUPPORT_CHAT_LINK = ""
     DOWNLOAD_DIRECTORY = "./downloads/"
